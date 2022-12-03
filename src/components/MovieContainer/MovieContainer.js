f65d33c600ebf3baaa722016c6a1842e71c000a0
Lauren Black <laurenblack5280@gmail.com>
2022-12-03 13:38:26 -0700

    add movie details mock data

--- src/components/MovieContainer/MovieContainer.js
+++ src/components/MovieContainer/MovieContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import MovieCard from '../MovieCard/MovieCard'
+import singleMovie from 'src/mockData/singleMovieData.js'
 
 class MovieContainer extends Component {
     constructor(props) {
