990a559ca4014cb2bb62f4598acc7796e0cf92f5
Dani Lopez-Cruz <roberto.lc2493@gmail.com>
2022-12-04 11:10:02 -0700

    add css changes to the movie container and cards

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,17 +1,17 @@
-import React from 'react'
-import "./MovieCard.css"
+import React from "react";
+import "./MovieCard.css";
 
-const MovieCard = ({id, title, poster, avgRating, releaseDate}) => {
-    return (
-        <section className='movie-card'>
-            <div className='movie-card-poster-container'>
-                <img src={poster} alt="movie poster"/>
-            </div>
-            <h3>{title}</h3>
-            <span>{releaseDate}</span>
-            <span>{avgRating}</span>
-        </section>
-    )
-}
+const MovieCard = ({ id, title, poster, avgRating, releaseDate }) => {
+  return (
+    <section className="movie-card">
+      <div className="movie-card-poster-container">
+        <span>{avgRating}</span>
+        <img src={poster} alt="movie poster" />
+      </div>
+      <h3>{title}</h3>
+      <span>{releaseDate}</span>
+    </section>
+  );
+};
 
-export default MovieCard
+export default MovieCard;
--- src/components/MovieContainer/MovieContainer.js
+++ src/components/MovieContainer/MovieContainer.js
@@ -1,27 +1,22 @@
-import React from 'react'
-import MovieCard from '../MovieCard/MovieCard'
-import "./MovieContainer.css"
+import React from "react";
+import MovieCard from "../MovieCard/MovieCard";
+import "./MovieContainer.css";
 
-const MovieContainer = ({movieData}) => {
-    const movieCards = movieData.map(movie => {
-        return (
-            <MovieCard 
-                id={movie.id}
-                key={movie.id}
-                poster={movie.poster_path}
-                title={movie.title}
-                avgRating={movie.average_rating.toFixed(0)}
-                releaseDate={movie.release_date}
-            />
-        )
-    })
-    console.log(movieCards)
+const MovieContainer = ({ movieData }) => {
+  const movieCards = movieData.map((movie) => {
     return (
-        <main className='movie-container'>
-            {movieCards}
-        </main>
-    )
-}
+      <MovieCard
+        id={movie.id}
+        key={movie.id}
+        poster={movie.poster_path}
+        title={movie.title}
+        avgRating={movie.average_rating.toFixed(0)}
+        releaseDate={movie.release_date}
+      />
+    );
+  });
+  console.log(movieCards);
+  return <main className="movie-container">{movieCards}</main>;
+};
 
-
-export default MovieContainer
+export default MovieContainer;
