30f22150aadcdbc140f35f93be307dcbe7749669
Lauren Black <laurenblack5280@gmail.com>
2022-12-05 19:51:33 -0700

    updated componentDidMount with network request, add error handling

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,25 +4,44 @@ import Header from "../Header/Header";
 import MovieContainer from "../MovieContainer/MovieContainer";
 import movieData from "../../mockData/movieData";
 import Hero from "../Hero/Hero";
+import { getMovies } from "../../apiCalls";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      movies: movieData.movies,
+      allMovies: movieData.movies,
+      movies: [],
       movieForHero: {},
+      error: ''
     };
   }
 
   randomMoviePoster = () => {
-    const index = Math.floor(Math.random() * this.state.movies.length);
-    const randomMovie = this.state.movies[index];
+    const index = Math.floor(Math.random() * this.state.allMovies.length);
+    const randomMovie = this.state.allMovies[index];
     return randomMovie;
   };
+
+  componentDidMount = () => {
+    getMovies()
+    .then(data => {
+      this.setState({
+        movies: data.movies
+      })
+    })
+    .catch(err => {
+      console.log(`${err.name}, ${err.message}`)
+      this.setState({
+        error: "Sorry, something went wrong"
+      })
+    })
+  }
 
   render() {
     return (
       <div className="App">
+        {this.state.error && <h2>{this.state.error}</h2>}
         <Header />
         <Hero randomMovie={this.randomMoviePoster()} />
         <MovieContainer movieData={this.state.movies} />
