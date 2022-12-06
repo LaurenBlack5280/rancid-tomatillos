017a1158e2c6aff3559c036d07160f7ed5616490
Dani Lopez-Cruz <roberto.lc2493@gmail.com>
2022-12-06 14:40:12 -0700

    refactor: condense fetch calls to one

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,5 @@
-const getMovies = () => {
-    return fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
+const fetchMovieData = (endpoint) => {
+    return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies${endpoint}`)
     .then(res => {
       if(!res.ok) {
         throw new Error()
@@ -7,15 +7,4 @@ const getMovies = () => {
       return res.json()
     })
 }
-
-const getSingleMovie = () => {
-    return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${this.state.id}`)
-    .then(res => {
-      console.log('response', res)
-      if(!res.ok) {
-        throw new Error()
-      }
-      return res.json()
-    })
-}
-export { getMovies, getSingleMovie }
+export { fetchMovieData }
