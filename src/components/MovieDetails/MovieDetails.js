9593887fd2d1d83e953fab7f065df32cda7860da
Dani Lopez-Cruz <roberto.lc2493@gmail.com>
2022-12-10 01:59:53 -0700

    initialize JSX architecture in MovieDetails.js

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { fetchMovieData } from "../../apiCalls";
+import "./MovieDetails.css";
 
 class MovieDetails extends Component {
   constructor(props) {
@@ -8,37 +9,52 @@ class MovieDetails extends Component {
       id: props.id,
       movie: {},
       error: "",
+      backdrop: {},
     };
+  }
+
+  styleBackdrop = () => {
+
   }
 
   componentDidMount = () => {
     fetchMovieData(`/${this.state.id}`)
-    .then(data => {
-        this.setState({movie: data.movie})
-    })
-    .catch( err => {
-        this.setState({error: err.status})
-    })
-  }
+      .then((data) => {
+        // also setState to backdrop, by invoking styleBackdrop() here
+        this.setState({ movie: data.movie });
+      })
+      .catch((err) => {
+        this.setState({ error: err.status });
+      });
+  };
 
   render() {
     return (
-      <article>
-        <h2>{this.state.movie.title}</h2>
-        <img src={this.state.movie.poster_path} alt="movie poster" />
-        <img src={this.state.movie.backdrop_path} alt="movie backdrop" />
-        <p>
-          <span>{this.state.movie.release_date} </span>
-          <span>{this.state.movie.overview}</span>
-          <span>{this.state.movie.average_rating}</span>
-          <span>{this.state.movie.genres}</span>
-          <span>{this.state.movie.budget}</span>
-          <span>{this.state.movie.revenue}</span>
-          <span>{this.state.movie.runtime}</span>
-          <span>{this.state.movie.tagline}</span>
-        </p>
-        <button onClick={() => this.props.goBackHome(true)}>Go back to all movies</button>
-      </article>
+      <section className="movie-details-page" style={this.state.backdrop}>
+        <div className="details-background">
+          <div className="movie-details-container">
+            <div className="movie-poster-container">
+              <div className="movie-poster">
+                <img src={this.state.movie.poster_path} alt="movie poster" />
+              </div>
+            </div>
+            <div className="movie-descriptions-container">
+              <h2 className="movie-details-movie-title">{this.state.movie.title}</h2>
+              <img src={this.state.movie.backdrop_path} alt="movie backdrop" />
+              <p>
+                <span>{this.state.movie.release_date} </span>
+                <span>{this.state.movie.overview}</span>
+                <span>{this.state.movie.average_rating}</span>
+                <span>{this.state.movie.genres}</span>
+                <span>{this.state.movie.budget}</span>
+                <span>{this.state.movie.revenue}</span>
+                <span>{this.state.movie.runtime}</span>
+                <span>{this.state.movie.tagline}</span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
     );
   }
 }
