74ae6b50dbc0d75ae67a3b4200c0b9c5ac1e460c
Dani Lopez-Cruz <roberto.lc2493@gmail.com>
2022-12-13 01:49:40 -0700

    Add prettier formatting

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,10 +1,9 @@
 const fetchMovieData = (endpoint) => {
-    return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies${endpoint}`)
-    .then(res => {
-      if(!res.ok) {
-        throw new Error()
-      }
-      return res.json()
-    })
-}
-export { fetchMovieData }
+  return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies${endpoint}`).then((res) => {
+    if (!res.ok) {
+      throw new Error();
+    }
+    return res.json();
+  });
+};
+export { fetchMovieData };
